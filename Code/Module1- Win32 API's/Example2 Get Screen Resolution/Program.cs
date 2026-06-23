e8ce20fedef63b614f566e143575b2f11ba2b3e3
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-06-23 22:13:44 +0300

    Module1 -> Example2_Get_Screen_Resolution

---
+++ Code/Module1- Win32 API's/Example2 Get Screen Resolution/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace Example2_Get_Screen_Resolution
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+        static void Main(string[] args)
+        {
+            int screenWidth = GetSystemMetrics(0);
+            int screenHeight = GetSystemMetrics(1);
+            Console.WriteLine("Screen Width: {0}, Screen Height: {1}", screenWidth, screenHeight);
+        }
+    }
+}

e97c5b527456b01e07d6783d24e80bb7c72cfa3b
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-06-23 21:46:31 +0300

    Module1 -> Example1_Change_Desktop_Wallpaper

---
+++ Code/Module1- Win32 API's/Example1 Change Desktop Wallpaper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Example1_Change_Desktop_Wallpaper
+{
+    class Program
+    {
+        // Import the SystemParametersInfo function from user32.dll
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(
+            uint action, uint uParam, string vParam, uint winIni);
+
+        // Constants for the function
+        public static readonly uint SPI_SETDESKWALLPAPER = 0x14;
+        public static readonly uint SPIF_UPDATEINIFILE = 0x01;
+        public static readonly uint SPIF_SENDCHANGE = 0x02;
+
+        static void Main()
+        {
+            // The path to the wallpaper image
+            string wallpaperPath = @"D:\WhatsApp Image 2026-05-26 at 3.30.38 PM.jpeg";
+
+            // Set the wallpaper
+            SetWallpaper(wallpaperPath);
+            Console.ReadKey();
+
+        }
+
+        public static void SetWallpaper(string path)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+            Console.WriteLine("Wallpaper changed successfully!");
+        }
+    }
+
+}

63fde054f421af4d5840cec2cf7ec13942e7db77
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-06-24 22:58:11 +0300

    Module1 -> Example5_MessageBox

---
+++ Code/Module1- Win32 API's/Example5 MessageBox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Example5_MessageBox
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern int MessageBox(IntPtr hWnd, string Text, string Caption, uint Type);
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero, "Hello, World!", "Message Box Example", 0);
+        }
+    }
+}

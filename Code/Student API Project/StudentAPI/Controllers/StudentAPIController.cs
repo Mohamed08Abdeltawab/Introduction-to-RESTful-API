88aaaacf4a12b9c6b3dcffa7943190757f3a7922
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-07-02 11:58:42 +0300

    Module3 -> 01 Student API Project v1 - Get ALL -> Server Side Code

---
+++ Code/Student API Project/StudentAPI/Controllers/StudentAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAPI.DataSimulation;
+using System.Collections.Generic;
+using StudentAPI.Models;
+
+namespace StudentAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/StudentAPI")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
+        {
+            var students = StudentDataSimulation.StudentsList;
+            return Ok(students);
+        }
+    }
+}
---
+++ Code/Student API Project/StudentAPI/DataSimulation/StudentDataSimulation.cs
@@ -0,0 +1,16 @@
+﻿using StudentAPI.Models;
+
+namespace StudentAPI.DataSimulation
+{
+    public class StudentDataSimulation
+    {
+        public static readonly List<Student> StudentsList = new List<Student>
+        {
+            new Student { Id = 1, Name = "Alice", Age = 20, Grade = 90 },
+            new Student { Id = 2, Name = "Bob", Age = 22, Grade = 85 },
+            new Student { Id = 3, Name = "Charlie", Age = 21, Grade = 88 },
+            new Student { Id = 4, Name = "David", Age = 23, Grade = 92 },
+            new Student { Id = 5, Name = "Eve", Age = 20, Grade = 87 }
+        };
+    }
+}
